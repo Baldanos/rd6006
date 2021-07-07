6a0b85bbc3673de8828c0132e0d9570717d0bb2a
natecostello <natecostello@gmail.com>
2021-07-06 21:22:31 -0400

    Fixed install errors.
    The proposed changes fixed module import errors when trying to install wiith pip from the repository.  Details in the comments.

--- rd6006/__init__.py
+++ rd6006/__init__.py
@@ -1,4 +1,9 @@
-from .rd6006 import *
+# The following results in import errors when I run
+# python3 -m pip install -v -v -v git+https://github.com/Baldanos/rd6006.git
+# due to dependandies not being installed yet:
+# ModuleNotFoundError: No module named 'minimalmodbus'
+
+# from .rd6006 import *
 
 name = "rd6006"
 __version__ = 0.2
